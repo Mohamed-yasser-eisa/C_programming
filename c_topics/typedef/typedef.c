300b4f56b582c403a36bf9d622e16d4756f473a8
Mohamed Yasser <engmohamedyasser8@gmail.com>
2022-04-23 03:30:01 +0200

    new

--- c_topics/typedef/typedef.c
+++ c_topics/typedef/typedef.c
@@ -10,6 +10,50 @@ typedef char* pointer_char;
 typedef int*  pointer_int;
 
 
+/*typedef with struct:*/
+/*we can use typedef to give a name to the struct as follow:*/
+typedef struct car
+{
+    int x;
+    short y;
+}car_t;
+
+/*Or you can give a name to struct after struct declaration as follow:*/
+struct bus
+{
+    char a;
+    int b;
+};
+
+typedef struct bus bus; /*now you can use "bus" to create a new variable from struct bus.*/
+
+
+/*Also you can use typedef with struct without tag_name, as follow:*/
+typedef struct
+{
+    int id;
+    char name[10];
+    short number;
+}client;  /*now client is a datatype keyword, and you can use it to create variable from struct datatype.*/
+
+
+
+/*****************tricks************************************/
+
+
+/*Also you can use typedef with pointer to function to create a new datatype from type: "pointer to function" as follow:*/
+typedef int (*ptr_fun)(int, int); /*now ptr_fun is a datatype of type pointer to function that takes two ints and returns int*/
+
+/*Also you can use typedef with pointer to array to create a new datatype from type: "pointer to array" as follow:*/
+typedef int (*ptr_arr)[4]; /*now ptr_arr is a datatype of type pointer to array of 4 ints.*/
+
+/*Also you can use typedef with array to create a new datatype from type: "array" as follow:*/
+typedef int arr[4]; /*now arr is a datatype of type array of 4 ints. Which means that array name is a datatype.*/
+
+/***********************************************************/
+
+
+
 
 int main()
 {
@@ -20,6 +64,12 @@ int main()
 
     Byte x = 255;
     printf("X = %i\n", x);
+
+    car_t c1;
+    c1.x = 50;
+    c1.y = 1000;
+    printf("c1.x = %i\n", c1.x);
+    printf("c1.y = %i\n", c1.y); 
 
 
 
