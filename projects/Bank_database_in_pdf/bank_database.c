413fef466238dff22f2f897c170b3ab006ae1289
Mohamed Yasser <engmohamedyasser8@gmail.com>
2022-04-26 15:14:24 +0200

    new

---
+++ projects/Bank_database_in_pdf/bank_database.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main()
+{
+    puts("************************************************");
+    int x = 0x00000000;
+    printf("~x = %#X\n", ~x);
+    printf("!x = %#X\n", !x);
+
+
+    puts("************************************************");
+    return 0;
+}
